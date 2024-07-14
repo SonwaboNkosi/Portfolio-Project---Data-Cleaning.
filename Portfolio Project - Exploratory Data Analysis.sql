17e4e5503847599212ff31207fa64e6270ae43d0
SonwaboNkosi <66822061+SonwaboNkosi@users.noreply.github.com>
2024-07-14 19:07:55 +0200

    Update Portfolio Project - Exploratory Data Analysis.sql

--- Portfolio Project - Exploratory Data Analysis.sql
+++ Portfolio Project - Exploratory Data Analysis.sql
@@ -1,5 +1,9 @@
 -- Exploratory Data Analysis 
+-- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
 
+-- normally when you start the EDA process you have some idea of what you're looking for
+
+-- with this info we are just going to look around and see what we find!
 
 SELECT *
 FROM layoffs_staging2;
